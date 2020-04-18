a319bf015c4eea5ec19719123f458382bb8d742e
naveen-iyer-13 <naveeniyer13@gmail.com>
2020-04-18 23:59:02 +0530

    Updates

---
+++ precache-manifest.c4541fb47c2026d84de935f84d2b985f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37b03fc88b7bfbdce06bd1378ccf5de",
+    "url": "/full-throttle-task/index.html"
+  },
+  {
+    "revision": "9b50e3a212599c2f136a",
+    "url": "/full-throttle-task/static/css/2.bfaf19e1.chunk.css"
+  },
+  {
+    "revision": "b517a920c397c58a4328",
+    "url": "/full-throttle-task/static/css/main.7f582baa.chunk.css"
+  },
+  {
+    "revision": "9b50e3a212599c2f136a",
+    "url": "/full-throttle-task/static/js/2.ddd12110.chunk.js"
+  },
+  {
+    "revision": "04136edc6c83cac026f5f722476ba727",
+    "url": "/full-throttle-task/static/js/2.ddd12110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b517a920c397c58a4328",
+    "url": "/full-throttle-task/static/js/main.c5fcc6e4.chunk.js"
+  },
+  {
+    "revision": "5ec4870115908c865db2",
+    "url": "/full-throttle-task/static/js/runtime-main.12c9a752.js"
+  }
+]);
